b593cbe380e2343d2d09ebc7e6119f438bbbc774
sihyan <hsq13965557679@163.com>
2024-05-13 09:59:19 +0800

    Initial commit

---
+++ src/main/java/com/norm/CodeNormApplication.java
@@ -0,0 +1,13 @@
+package com.norm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeNormApplication.class, args);
+    }
+
+}
